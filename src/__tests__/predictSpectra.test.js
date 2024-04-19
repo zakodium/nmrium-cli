d7c0e7223c8bc0fefc7bf8726923c421873358a2
Luc Patiny <luc@patiny.com>
2024-04-19 07:57:31 +0200

    test: refactor predict Spectra test

--- src/__tests__/predictSpectra.test.js
+++ src/__tests__/predictSpectra.test.js
@@ -1,12 +1,10 @@
-import { statSync } from 'fs';
-import { readdir } from 'fs/promises';
 import { join } from 'path';
 
 import { expect, test } from 'vitest';
 
 import { predictSpectra } from '../commands/predictSpectra.js';
 
-const dataDir = join(__dirname, 'predicted');
+const dataDir = join(__dirname, 'toPredict');
 
 test('predictSpectra', async () => {
   await predictSpectra(__dirname, { dataDir });
